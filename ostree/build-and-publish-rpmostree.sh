b723591724421a49e69f8ed560ec97aa75767de9
orowith2os <93224879+orowith2os@users.noreply.github.com>
2022-11-13 21:22:15 -0600

    Add directory checks for rpmostree script, set all scripts as executable

--- ostree/build-and-publish-rpmostree.sh
+++ ostree/build-and-publish-rpmostree.sh
@@ -1,2 +1,17 @@
 #!/usr/bin/env bash
 
+if [ ! -d "$REPO_DIR" ]
+then
+	echo "Invalid repository directory."
+fi
+
+if [ ! -d "$CACHE_DIR" ]
+then
+	echo "Invalid cache directory."
+fi
+
+sudo ostree init --repo="REPO_DIR" --mode=$OSTREE_MODE
+
+sudo rpm-ostree compose tree --repo="REPO_DIR" --cachedir=$CACHE_DIR config.yml
+
+sudo ostree summary --repo="REPO_DIR"
